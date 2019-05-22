2aa3f928146a92d11bdb867e93b3333ca9002b17
Nicholas Villa <nicholasvilla@Nicholass-MacBook-Pro.local>
2019-05-21 20:39:06 -0400

    Updated recon entry point to separate logical parse function from top-level reconcile

--- recon/recon.py
+++ recon/recon.py
@@ -1,37 +1,42 @@
-#%%
 from models import Position, Transaction, Portfolio
 import re
 
-def read_file():
-    with open('./data/recon.in', 'r') as f:
-        sections = f.read().split('\n\n')
-        data = [s.strip().split('\n') for s in sections]
-    return data
-
-if __name__ == '__main__':
-    ## Usage:
-    #%%
+def parse_portfolio_text(prtf_text):
+    sections = prtf_text.split('\n\n')
+    data = [s.strip().split('\n') for s in sections]
     
-    #%%
-    data = read_file()
-    header_re = re.compile(r'D(\d+)-(POS|TRN)')
+    header_pattern = re.compile(r'D(\d+)-(POS|TRN)')
     positions = {}
     transactions = {}
     for section in data:
         header_str = section[0]
-        day_idx, inp_type = header_re.search(header_str).groups()
-        if inp_type == 'POS':
+        day_idx, input_type = header_pattern.search(header_str).groups()
+        day_idx = int(day_idx)
+        if input_type == 'POS':
             positions[day_idx] = [Position.from_text(s) for s in section[1:]]
-        elif inp_type == 'TRN':
+        elif input_type == 'TRN':
             transactions[day_idx] = [Transaction.from_text(s) for s in section[1:]]
 
-    #%%
-    initial_day_idx = '0'
-    portfolio = Portfolio.from_positions(positions[initial_day_idx])
+    return positions, transactions
 
-    for day_idx, trns_for_day in transactions.items():
-        for trn in trns_for_day:
-            portfolio.process_transaction(trn)
+def reconcile(initial_positions_day=0, compare_to_day=1, in_file='./data/recon.in', out_file='./data/recon.out'):
+    with open(in_file, 'r') as f:
+        text = f.read()
+    
+    positions, transactions = parse_portfolio_text(text)
+    initial_portfolio = Portfolio.from_positions(positions[initial_positions_day])
+    latest_prtf_state = Portfolio.from_positions(positions[compare_to_day])
 
-    print('Calculated positions:')
-    print(portfolio)
+    for day_idx in range(initial_positions_day, compare_to_day + 1):
+        if day_idx in transactions:
+            for trn in transactions[day_idx]:
+                initial_portfolio.process_transaction(trn)
+    
+    prtf_diff = initial_portfolio.reconcile(latest_prtf_state)
+
+    with open(out_file, 'w') as f:
+        for ticker, diff in prtf_diff.items():
+            f.write('{} {}\n'.format(ticker, diff))
+
+if __name__ == '__main__':
+    reconcile()
